5219e7adb3f7701318e76e9a65e627d7bf2648b8
Матвей <motysha1905@gmail.com>
2025-07-04 15:16:34 +0300

    Add main.py file

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes 
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send.message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Я умею напоминать o подписах. Команлы: /start, /add, /list, /delete, help.")
+
+def main():
+    if TOKEN is None:
+        print("Ошибка: Токен бота не найденю Установите переменную окружения")
+        return
+    
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
