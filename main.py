7d55a78d09c4da933356667eca777fe671849481
Матвей <motysha1905@gmail.com>
2025-07-05 13:57:12 +0300

    Add cancel command and finished conversation_handler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()      
 import os
 import logging
+from datetime import datetime
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -45,7 +46,7 @@ async def add_service_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -
     context.user_data['service_name'] = update.message.text
     await update.message.reply_text(f'Хорошо, "{context.user_data["service_name"]}". Теперь введите сумму платежа (например), "9.99"):')
     return ADD_AMOUNT
-async def add_ammount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def add_amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     try:
         ammount = float(update.message.text.replace('.', '.'))
         context.user_data['amount'] = ammount
@@ -54,8 +55,34 @@ async def add_ammount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
     except ValueError:
         await update.message.reply_text('Это не похоже на число. Пожалуйста, введите сумму цифрами (например, "9.99"):')
         return ADD_AMOUNT
+    
+async def add_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    date_str = update.message.text
+    try:
+        datetime.datetime.strptime(date_str, '%Y-%m-$d')
+        add_subscription(
+            user_id=update.effective_user.id,
+            service_name=context.user_data['service_name'],
+            amount=context.user_data['amount']
+            next_payment_date=date_str
+        )
+        await update.message.reply_text(
+            f"Отлично! Подписка '{context.user_data['service_name']}' на сумму {context.user_data['amount']} rub. "
+            f"со следующей оплатой {date_str} добавлена."
+        )
+        return ConversationHandler.END
+    except ValueError:
+        await update.message.reply_text('Неверный формат даты. Пожалуйста, введите дату в формате ГГГГ-ММ-ДД:')
+        return ADD_DATE
+    
+async def cancel_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text('Операция отменена. Теперь можете начать заново.')
+    return ConversationHandler.END
 
 def main():
+    create_table()
+    logging.info("База данных и таблица подписок проверены.созданы.")
+
     if TOKEN is None:
         print("Ошибка: Токен бота не найденю Установите переменную окружения")
         return
@@ -64,11 +91,22 @@ def main():
 
     start_handler = CommandHandler('start', start)
     help_handler = CommandHandler('help', help)
+    add_conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('add', add_start)]
+        states={
+            ADD_SERVICE_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_service_name)],
+            ADD_AMOUNT: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_amount)],
+            ADD_DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_date)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel_command)]
+    )
+
     application.add_handler(start_handler)
     application.add_handler(help_handler)
+    application.add_handler(add_conversation_handler)
 
-    print("Бот запущен. Ctrl+C для остановки работы.")
-    application.run_polling()
+    logging.info("Бот запущен. Ctrl+C для остановки работы.")
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
